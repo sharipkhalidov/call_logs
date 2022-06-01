1cdf988df01c23cc2e9d7ec76789b141ec6c2908
sharipkhalidov <sharip.khalidov@mail.ru>
2022-06-01 20:54:52 +0300

    les 93 json

--- lib/recent_calls/call_info_screen.dart
+++ lib/recent_calls/call_info_screen.dart
@@ -1,6 +1,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../styles/colors.dart';
+
+
 class CallInfo extends StatelessWidget {
   const CallInfo({Key? key}) : super(key: key);
 
@@ -11,10 +16,101 @@ class CallInfo extends StatelessWidget {
         color: Colors.orange[300],
         alignment: Alignment.center,
         child: FutureBuilder<http.Response>(
-          future: http.get(Uri.parse('https://catfact.ninja/fact')),
+          future: http.get(Uri.parse('https://reqres.in/api/users/5')),
           builder: (BuildContext context,AsyncSnapshot<http.Response> snapshot) {
+
             if (snapshot.hasData) {
-    return Text(snapshot.data!.body);
+               final decode = jsonDecode(snapshot.data!.body);
+               final user_info = decode['data'];
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 20),
+            Text(
+              'id',
+              style: const TextStyle(
+                  fontSize: 30, color: AppColor.link),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Text(user_info['id'].toString(),
+                  style: const TextStyle(
+                      fontSize: 30, color: AppColor.secondary)),
+            ),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 20),
+            Text(
+              'email',
+              style: const TextStyle(
+                  fontSize: 30, color: AppColor.primary),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Text(user_info['email'].toString(),
+                  style: const TextStyle(
+                      fontSize: 30, color: AppColor.secondary)),
+            ),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 20),
+            Text(
+              'first_name',
+              style: const TextStyle(
+                  fontSize: 30, color: AppColor.primary),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Text(user_info['first_name'].toString(),
+                  style: const TextStyle(
+                      fontSize: 30, color: AppColor.secondary)),
+            ),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 20),
+            Text(
+              'last_name',
+              style: const TextStyle(
+                  fontSize: 30, color: AppColor.primary),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Text(user_info['last_name'].toString(),
+                  style: const TextStyle(
+                      fontSize: 30, color: AppColor.secondary)),
+            ),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 20),
+            Text(
+              'avatar',
+              style: const TextStyle(
+                  fontSize: 30, color: AppColor.primary),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Text(user_info['avatar'].toString(),
+                  style: const TextStyle(
+                      fontSize: 30, color: AppColor.secondary)),
+            ),
+          ],
+        ),
+      ],
+    );
     } else {
               return const CircularProgressIndicator();
             }
@@ -23,4 +119,14 @@ class CallInfo extends StatelessWidget {
       );
 
   }
+}
+
+
+class Post{
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body);
 }
